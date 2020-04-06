6303b9192a29f5273f750e7396de6fcf2e885eac
Ben Oxley <ben.oxley@gmail.com>
2020-04-06 13:36:37 +0100

    Added test cases

---
+++ COVIDTests/PersonGeneratorTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using COVID;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COVID.Tests
+{
+    [TestClass()]
+    public class PersonGeneratorTests
+    {
+       
+
+        [TestMethod()]
+        public async Task GetNumCasesTestAsync()
+        {
+            await PersonGenerator.GetNumCases();
+        }
+    }
+}
